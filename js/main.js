8112e045d2a102cf43817bf6a4748dd3c2b2df70
dholm90 <devanholm@gmail.com>
2022-07-13 00:47:36 -0400

    Functional slider for grid size

--- js/main.js
+++ js/main.js
@@ -1,4 +1,54 @@
 const main = document.querySelector('main');
+const body = document.querySelector('body');
+
+const header = document.createElement('header');
+const slider = document.createElement('div');
+const button = document.createElement('button');
+const sliderNum = document.createElement('span');
+
+slider.classList.add('slidecontainer');
+slider.innerHTML = "<h2>Grid Size:</h2>";
+sliderNum.classList.add('sliderNum');
+slider.innerHTML += "<input type='range' min='1' max='100' value='16' class='slider' id='myRange'>";
+
+button.textContent = "Create Grid";
+
+button.addEventListener('click', (range) => {
+    Clear();
+    range = document.getElementById("myRange").value;
+    CreateGrid(range);
+    const articles = document.querySelectorAll('.article');
+
+    articles.forEach((article) => {
+        article.addEventListener('mouseover', () => {
+            article.classList.add('articleHover');
+        });
+    });
+});
+
+body.insertBefore(header, body.children[0]);
+
+header.innerHTML = '<h1>Etch-A-Sketch</h1>';
+header.appendChild(slider);
+header.appendChild(sliderNum);
+header.appendChild(button);
+
+//let rangeSlider = document.getElementById("myRange");
+//let output = document.querySelector("sliderNum");
+///output.innerHTML = rangeSlider.value; //Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+
+
+//CreateGrid(rangeSlider.value);
+
+// const articles = document.querySelectorAll('.article');
+
+// articles.forEach((article) => {
+//     article.addEventListener('mouseover', () => {
+//         article.classList.add('articleHover');
+//     });
+// });
 
 function CreateGrid(squares) {
     sqrtOfSquares = Math.sqrt(squares);
@@ -18,14 +68,14 @@ function CreateGrid(squares) {
     }
 }
 
-CreateGrid(144);
-
-const articles = document.querySelectorAll('.article');
-
-articles.forEach((article) => {
-    article.addEventListener('mouseover', () => {
-        article.classList.add('articleHover');
+function Clear() {
+    const del = document.querySelectorAll('.section');
+    del.forEach(article => {
+        article.remove();
     });
-});
+}
+
+
+
 
 
