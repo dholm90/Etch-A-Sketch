6ca9f91fe2186b526a55e878a308ba045bf0848b
dholm90 <devanholm@gmail.com>
2022-07-12 23:28:21 -0400

    Create any sized grid with event listeners and basic styling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
 const main = document.querySelector('main');
 
-
 function CreateGrid(squares) {
     sqrtOfSquares = Math.sqrt(squares);
     for (i = 0; i < sqrtOfSquares; i++) {
         const section = document.createElement('div');
         section.classList.add('section');
+
         for (j = 0; j < sqrtOfSquares; j++) {
             const article = document.createElement('div');
             section.appendChild(article);
             article.classList.add('article');
+            article.addEventListener('mouseOver', () => {
+                article.classList.add('articleHover');
+            });
+
         }
         main.appendChild(section);
     }
 }
 
-CreateGrid(16);
+
+CreateGrid(144);
+
+const articles = document.querySelectorAll('.article');
+
+articles.forEach((article) => {
+    article.addEventListener('mouseover', () => {
+        article.classList.add('articleHover');
+    });
+});
+
+
