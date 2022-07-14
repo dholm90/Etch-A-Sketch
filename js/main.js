9bf20087ae9a783be1bf8f70d31ae0141eef8f6e
dholm90 <devanholm@gmail.com>
2022-07-13 20:41:00 -0400

    Project Completed

--- js/main.js
+++ js/main.js
@@ -5,13 +5,20 @@ const header = document.createElement('header');
 const slider = document.createElement('div');
 const button = document.createElement('button');
 const sliderNum = document.createElement('span');
+const h2 = document.createElement('h2');
 
+h2.textContent = "Grid Size";
 slider.classList.add('slidecontainer');
-slider.innerHTML = "<h2>Grid Size:</h2>";
 sliderNum.classList.add('sliderNum');
 slider.innerHTML += "<input type='range' min='1' max='100' value='16' class='slider' id='myRange'>";
 
 button.textContent = "Create Grid";
+sliderNum.innerText = "16 x 16";
+
+slider.addEventListener('input', (num) => {
+    num = document.getElementById("myRange").value;
+    sliderNum.innerText = num + ' x ' + num;
+});
 
 button.addEventListener('click', (range) => {
     Clear();
@@ -29,34 +36,19 @@ button.addEventListener('click', (range) => {
 body.insertBefore(header, body.children[0]);
 
 header.innerHTML = '<h1>Etch-A-Sketch</h1>';
-header.appendChild(slider);
+header.appendChild(h2);
 header.appendChild(sliderNum);
+header.appendChild(slider);
+
 header.appendChild(button);
 
-//let rangeSlider = document.getElementById("myRange");
-//let output = document.querySelector("sliderNum");
-///output.innerHTML = rangeSlider.value; //Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-
-
-//CreateGrid(rangeSlider.value);
-
-// const articles = document.querySelectorAll('.article');
-
-// articles.forEach((article) => {
-//     article.addEventListener('mouseover', () => {
-//         article.classList.add('articleHover');
-//     });
-// });
-
 function CreateGrid(squares) {
-    sqrtOfSquares = Math.sqrt(squares);
-    for (i = 0; i < sqrtOfSquares; i++) {
+
+    for (i = 0; i < squares; i++) {
         const section = document.createElement('div');
         section.classList.add('section');
 
-        for (j = 0; j < sqrtOfSquares; j++) {
+        for (j = 0; j < squares; j++) {
             const article = document.createElement('div');
             section.appendChild(article);
             article.classList.add('article');
